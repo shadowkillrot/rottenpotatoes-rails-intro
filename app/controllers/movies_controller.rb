d97094effce80b9e58ad168b563a95898eff24f5
shadowkillrot <218139640+shadowkillrot@users.noreply.github.com>
2025-10-02 21:47:56 +0500

    Pfixes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,48 +6,48 @@ class MoviesController < ApplicationController
   end
 
   def index
-  @all_ratings = Movie.all_ratings
-  
-  # Check if we need to restore from session and redirect
-  redirect_needed = false
-  
-  if params[:ratings].nil? && session[:ratings].present?
-    params[:ratings] = session[:ratings]
-    redirect_needed = true
+    @all_ratings = Movie.all_ratings
+    
+    # Check if we need to restore from session and redirect
+    redirect_needed = false
+    
+    if params[:ratings].nil? && session[:ratings].present?
+      params[:ratings] = session[:ratings]
+      redirect_needed = true
+    end
+    
+    if params[:sort_by].nil? && session[:sort_by].present?
+      params[:sort_by] = session[:sort_by]
+      redirect_needed = true
+    end
+    
+    # If we restored from session, redirect to RESTful URL with params visible
+    if redirect_needed
+      redirect_to movies_path(sort_by: params[:sort_by], ratings: params[:ratings]) and return
+    end
+    
+    # Handle ratings filter
+    if params[:ratings].present?
+      @ratings_to_show = params[:ratings].keys
+      session[:ratings] = params[:ratings]
+    else
+      @ratings_to_show = @all_ratings
+      session[:ratings] = Hash[@all_ratings.map { |r| [r, "1"] }]
+    end
+    
+    # Get movies with ratings filter
+    @movies = Movie.with_ratings(@ratings_to_show)
+    
+    # Handle sorting
+    if params[:sort_by].present?
+      @sort_by = params[:sort_by]
+      @movies = @movies.order(@sort_by)
+      session[:sort_by] = params[:sort_by]
+    else
+      @sort_by = nil
+      session.delete(:sort_by)
+    end
   end
-  
-  if params[:sort_by].nil? && session[:sort_by].present?
-    params[:sort_by] = session[:sort_by]
-    redirect_needed = true
-  end
-  
-  # If we restored from session, redirect to RESTful URL with params visible
-  if redirect_needed
-    redirect_to movies_path(sort_by: params[:sort_by], ratings: params[:ratings]) and return
-  end
-  
-  # Handle ratings filter
-  if params[:ratings].present?
-    @ratings_to_show = params[:ratings].keys
-    session[:ratings] = params[:ratings]
-  else
-    @ratings_to_show = @all_ratings
-    session[:ratings] = Hash[@all_ratings.map { |r| [r, "1"] }]
-  end
-  
-  # Get movies with ratings filter
-  @movies = Movie.with_ratings(@ratings_to_show)
-  
-  # Handle sorting
-  if params[:sort_by].present?
-    @sort_by = params[:sort_by]
-    @movies = @movies.order(@sort_by)
-    session[:sort_by] = params[:sort_by]
-  else
-    @sort_by = nil
-    session.delete(:sort_by)
-  end
-end
 
   def new
   end
